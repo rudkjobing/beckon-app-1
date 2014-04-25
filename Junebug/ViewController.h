06ab444bfadd843939bd3e40f972940b325a9472
Steffen Rudkjøbing <steffen@macbook-pro-2.home>
2014-04-25 21:34:55 +0200

    Initial Commit

---
+++ Junebug/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Junebug
+//
+//  Created by Steffen Rudkjøbing on 25/04/14.
+//  Copyright (c) 2014 General Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ JunebugTests/JunebugTests.m
@@ -0,0 +1,34 @@
+//
+//  JunebugTests.m
+//  JunebugTests
+//
+//  Created by Steffen Rudkjøbing on 25/04/14.
+//  Copyright (c) 2014 General Bits. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JunebugTests : XCTestCase
+
+@end
+
+@implementation JunebugTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
