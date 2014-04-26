23ac5b88efd136add5e02c04b4b43d6c716b84d4
Steffen Rudkjøbing <steffen@macbook-pro-2.home>
2014-04-26 16:28:17 +0200

    Added settings VC

---
+++ Junebug/SettingsVC.h
@@ -0,0 +1,13 @@
+//
+//  SettingsVC.h
+//  Junebug
+//
+//  Created by Steffen Rudkjøbing on 26/04/14.
+//  Copyright (c) 2014 General Bits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsVC : UIViewController
+
+@end
