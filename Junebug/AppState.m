13dea650ce74f2aef047d2876c653bc90319cd66
Steffen Rudkjøbing <steffen@MacBook-Pro-2.local>
2014-07-30 23:34:40 +0200

    HOTFIX: Spelling error corrected so that Beckons update.

--- Junebug/AppState.m
+++ Junebug/AppState.m
@@ -29,7 +29,7 @@
             }
         }
     }
-    else if([updateType isEqualToString:@"Beckons"]){
+    else if([updateType isEqualToString:@"Beckon"]){
         [self.beckons getUpdates];
     }
 }
